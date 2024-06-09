f0b3e064a5de1badd1ecbc87aac60387c568af6e
Great <greatochuko123@gmail.com>
2024-06-09 18:21:00 +0100

    create search form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/Header.module.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -15,6 +15,8 @@ const navLinks = [
 
 export default function Header() {
   const pathname = usePathname();
+  const [showSearchForm, setShowSearchForm] = useState(true);
+
   return (
     <div className={styles.header}>
       <div className={styles["header-container"]}>
@@ -32,6 +34,52 @@ export default function Header() {
               </Link>
             </li>
           ))}
+          <button
+            className={styles["search-btn"]}
+            onClick={() => setShowSearchForm((curr) => !curr)}
+          >
+            <svg
+              viewBox="0 -0.5 25 25"
+              height={28}
+              width={28}
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M5.5 10.7655C5.50003 8.01511 7.44296 5.64777 10.1405 5.1113C12.8381 4.57483 15.539 6.01866 16.5913 8.55977C17.6437 11.1009 16.7544 14.0315 14.4674 15.5593C12.1804 17.0871 9.13257 16.7866 7.188 14.8415C6.10716 13.7604 5.49998 12.2942 5.5 10.7655Z"
+                  stroke="#000000"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>{" "}
+                <path
+                  d="M17.029 16.5295L19.5 19.0005"
+                  stroke="#000000"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>{" "}
+              </g>
+            </svg>
+          </button>
+          <form
+            className={`${styles["search-form"]} ${
+              showSearchForm ? styles["visible"] : ""
+            }`}
+          >
+            <input type="text" />
+            <button type="submit">Search</button>
+          </form>
         </ul>
         <div className={styles["auth-links"]}>
           <Link href={"/login"} className={styles["login"]}>
