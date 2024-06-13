246efe3bcf15cf303b86d3906ff3f3814daf3cb8
Great <greatochuko123@gmail.com>
2024-06-13 02:59:13 +0100

    create header for search page and add dynamic title to seach page via generateMetadata

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,17 @@
+import SearchPageForm from "@/components/SearchPageForm";
 import React from "react";
+import styles from "@/styles/SearchPage.module.css";
+import { Metadata } from "next";
+
+export function generateMetadata({
+  searchParams,
+}: {
+  searchParams: { query: string };
+}): Metadata {
+  return {
+    title: `Search results for "${searchParams.query}"`,
+  };
+}
 
 export default function SearchPage({
   searchParams,
@@ -6,5 +19,13 @@ export default function SearchPage({
   searchParams: { query: string };
 }) {
   console.log(searchParams);
-  return <div>Search Results for "{searchParams.query}"</div>;
+
+  return (
+    <div>
+      <div className={styles["heading"]}>
+        <SearchPageForm searchQuery={searchParams.query} />
+        <h1>Search results for "{searchParams.query}"</h1>
+      </div>
+    </div>
+  );
 }
