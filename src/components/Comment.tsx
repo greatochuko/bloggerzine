a65f8c69e4ac381a68604456dde72839f4f1b8f3
Great <greatochuko123@gmail.com>
2024-06-14 14:33:05 +0100

    fix reply form width issue

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -19,51 +19,50 @@ export type CommentType = {
 export default function Comment({
   comment,
   comments,
-  isReply,
   replyFormId,
   setReplyFormId,
 }: {
   comment: CommentType;
   comments: CommentType[];
-  isReply?: boolean;
   replyFormId: number | null;
   setReplyFormId: React.Dispatch<React.SetStateAction<number | null>>;
 }) {
   const replies = comments.filter((c) => c.parentId === comment.id);
 
   return (
-    <li className={`${styles["comment"]} ${isReply ? styles["reply"] : ""}`}>
-      <div className={styles["image-container"]}>
-        <Image
-          src={comment.user.imageUrl}
-          alt={comment.user.name}
-          fill
-          sizes=""
-        ></Image>
+    <li className={styles["comment"]}>
+      <div className={styles["main"]}>
+        <div className={styles["image-container"]}>
+          <Image
+            src={comment.user.imageUrl}
+            alt={comment.user.name}
+            fill
+            sizes=""
+          ></Image>
+        </div>
+        <div className={styles["details"]}>
+          <h3>{comment.user.name}</h3>
+          <time>{comment.dateCreated}</time>
+          <p className={styles["content"]}>{comment.comment}</p>
+          <button onClick={() => setReplyFormId(comment.id)}>Reply</button>
+          {replyFormId === comment.id ? (
+            <ReplyForm close={() => setReplyFormId(null)} />
+          ) : null}
+        </div>
       </div>
-      <div className={styles["details"]}>
-        <h3>{comment.user.name}</h3>
-        <time>{comment.dateCreated}</time>
-        <p className={styles["content"]}>{comment.comment}</p>
-        <button onClick={() => setReplyFormId(comment.id)}>Reply</button>
-        {replyFormId === comment.id ? (
-          <ReplyForm close={() => setReplyFormId(null)} />
-        ) : null}
-        {/* {replies.length ? (
-          <ul className={styles["replies"]}>
-            {replies.map((comment) => (
-              <Comment
-                comment={comment}
-                key={comment.id}
-                comments={comments}
-                isReply
-                replyFormId={replyFormId}
-                setReplyFormId={setReplyFormId}
-              />
-            ))}
-          </ul>
-        ) : null} */}
-      </div>
+      {replies.length ? (
+        <ul className={styles["replies"]}>
+          {replies.map((comment) => (
+            <Comment
+              comment={comment}
+              key={comment.id}
+              comments={comments}
+              replyFormId={replyFormId}
+              setReplyFormId={setReplyFormId}
+            />
+          ))}
+        </ul>
+      ) : null}
     </li>
   );
 }
