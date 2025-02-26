caecef325ebf4f17ce77550dfa316632a2b3145f
Great Ogheneochuko <greatochuko123@gmail.com>
2025-02-26 12:59:05 +0100

    update Hero component for improved blogpost display and add ESLint rules

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,9 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import styles from "@/styles/Hero.module.css";
 import Image from "next/image";
-import Link from "next/link";
-import BlogMetaData from "./BlogMetaData";
-import { categories } from "./Category";
 import { UserType } from "@/services/userServices";
-import convertToUrl from "@/utils/convertToUrl";
 
 export type BlogpostType = {
   id: string;
@@ -22,38 +20,72 @@ export type BlogpostType = {
 };
 
 export default function Hero({ blogposts }: { blogposts: BlogpostType[] }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (currentIndex >= 2) {
+        setCurrentIndex(0);
+      } else {
+        setCurrentIndex(currentIndex + 1);
+      }
+    }, 5000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [currentIndex]);
+
+  const selectedBlog = blogposts[currentIndex];
+
   return (
     <div className={styles["hero"]}>
-      {blogposts.map((blogpost) => (
-        <div key={blogpost.id}>
-          <div className={styles["overlay"]}>
-            <p
-              className={styles["category"]}
-              style={{
-                backgroundColor: categories.find(
-                  (cat) =>
-                    cat.name.toLowerCase() === blogpost.category.toLowerCase()
-                )?.color,
-              }}
-            >
-              {blogpost.category}
-            </p>
-            <Link
-              href={`/blog/${convertToUrl(blogpost.title)}_${blogpost.id}`}
-              className={styles["blog-title"]}
-            >
-              {blogpost.title}
-            </Link>
-            <BlogMetaData blog={blogpost} />
-          </div>
+      {blogposts.map((blogpost, index) => (
+        <div
+          key={blogpost.id}
+          className={[
+            styles["blogpost"],
+            currentIndex === index ? styles["show"] : "",
+          ].join(" ")}
+        >
           <Image
-            src={blogpost.thumbnail || ""}
-            fill
+            src={blogpost.thumbnail}
             alt={blogpost.title}
-            sizes="(max-width: 1024px) 90vw, 50vw"
-          ></Image>
+            fill
+            sizes="90vw"
+          />
+          <div className={styles["overlay"]}>
+            <p>{selectedBlog.category}</p>
+            <h3>{selectedBlog.title}</h3>
+            <div className={styles["user"]}>
+              <Image
+                src={selectedBlog.author.imageUrl}
+                alt={`${selectedBlog.author.firstname}'s profile picture`}
+                width={44}
+                height={44}
+              />
+              <div className={styles["details"]}>
+                <h4>
+                  {selectedBlog.author.firstname} {selectedBlog.author.lastname}
+                </h4>
+                <p>
+                  {new Date(selectedBlog.createdAt).toDateString()} &middot;{" "}
+                  {selectedBlog.views} Reads
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
+      <div className={styles["indicators"]}>
+        {new Array(3).fill("").map((_, index) => (
+          <div
+            key={index}
+            className={currentIndex === index ? styles["active"] : ""}
+            onClick={() => setCurrentIndex(index)}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 }
